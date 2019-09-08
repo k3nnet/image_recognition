dcc6a49f0c17f53e18686a1fc8514aa028bcc429
k3nnet <kkmphele@gmail.com>
2019-09-08 12:25:06 +0200

    loaded models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, Inject } from '@angular/core';
+import * as faceapi from 'face-api.js';
+import {WebcamImage, WebcamInitError, WebcamUtil} from 'ngx-webcam';
+import {Subject,Observable} from 'rxjs';
+import * as canvas from 'canvas';
+import * as $ from 'jquery';
+import { DOCUMENT } from '@angular/common'; 
 
+const  Canvas = canvas.Canvas;
+const Image=canvas.Image;
+const ImageData=canvas.ImageData;
+
+faceapi.env.monkeyPatch({ImageData})
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @Input() cameraName:string = "";
+ // @ViewChild('myImage',{ static: false }) imageInput: HTMLVideoElement;
   title = 'imageRecognition';
+  // tiny_face_detector options
+inputSize = 512;
+scoreThreshold = 0.5;
+withFaceLandmarks = false;
+withBoxes = true;
+videoEl;
+stream;
+ // latest snapshot
+ public webcamImage: WebcamImage = null;
+ private doc:Document
+
+ // webcam snapshot trigger
+ private trigger: Subject<void> = new Subject<void>();
+ // switch to next / previous / specific webcam; true/false: forward/backwards, string: deviceId
+ private nextWebcam: Subject<boolean|string> = new Subject<boolean|string>();
+  private input:HTMLVideoElement
+constructor(@Inject(DOCUMENT) document
+) {
+  this.doc=document;
+}
+
+ngOnInit() {
+this.loadModels();
+
+}
+ngAfterViewInit() {
+  
+
+}
+public handleImage(webcamImage: WebcamImage): void {
+  console.info('received webcam image', webcamImage);
+  this.webcamImage = webcamImage;
+}
+
+ async loadModels() {
+  // load the models
+  const MODEL_URL = './assets/models/'
+
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
+  await faceapi.loadFaceRecognitionModel(MODEL_URL)
+  const input = <HTMLImageElement>this.doc.getElementById("myImage")
+  
+let fullFaceDescriptions = await faceapi.detectAllFaces(input).withFaceLandmarks().withFaceDescriptors()
+
+ 
+  }
+
+
+  
+  public get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<boolean|string> {
+    return this.nextWebcam.asObservable();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {WebcamModule} from 'ngx-webcam'
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    WebcamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
