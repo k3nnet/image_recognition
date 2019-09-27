7f07a789c6cb3cceeb4d201514324b272fd4ae2e
k3nnet <kkmphele@gmail.com>
2019-09-27 22:34:17 +0200

    added a nodejs server

--- server.js
+++ server.js
@@ -9,3 +9,9 @@ app.use(express.static(__dirname+"/dist/imageRecognition"));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+"/dist/imageRecognition/index.html"))
 })
+
+var port=process.env.PORT || 4000;
+
+app.listen(port,function(){
+    console.log('server started on port '+port)
+})
